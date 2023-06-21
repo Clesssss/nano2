ec229d43846e00c069955a3f4beefe18e809acb0
Michael Richard <c14220275@john.petra.ac.id>
2023-06-21 11:47:25 +0700

    michael richard

--- core/src/com/mygdx/game/Mushroom.java
+++ core/src/com/mygdx/game/Mushroom.java
@@ -1,6 +1,11 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 public class Mushroom extends Character{
+    Animation<TextureRegion> mIdleAnimation;
+
     @Override
     public void isAttacked(int damage) {
 
