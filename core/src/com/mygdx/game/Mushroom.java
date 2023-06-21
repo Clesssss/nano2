902fdbf4fb504cea85436c983b9267acfe326fe5
Michael Richard <c14220275@john.petra.ac.id>
2023-06-21 11:56:46 +0700

    michael richard

--- core/src/com/mygdx/game/Mushroom.java
+++ core/src/com/mygdx/game/Mushroom.java
@@ -3,8 +3,22 @@ package com.mygdx.game;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+import java.awt.*;
+
 public class Mushroom extends Character{
     Animation<TextureRegion> mIdleAnimation;
+    Animation<TextureRegion> mDeadAnimation;
+    Animation<TextureRegion> mIsHitAnimation;
+    private Animation<TextureRegion>mWalkAnimation;
+    private Animation<TextureRegion> mAtkAnimation;
+    private TextureRegion mCurrFrame;
+    private float mDeathTime;
+    private float mIsHitTime;
+    private boolean mIsHit;
+    private float mSTime;
+    private int x;
+    private int y;
+    private Rectangle mHitArea;
 
     @Override
     public void isAttacked(int damage) {
